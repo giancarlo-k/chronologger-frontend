c4162b6667a1b157936def048b93937af74e319d
Giancarlo Kite <103792674+giancarlo-k@users.noreply.github.com>
2025-04-08 18:06:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login.jsx';
-import Signup from './pages/signup.jsx';
+import Signup from './pages/Signup.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import LogInfoSkeleton from "./pages/LogInfoSkeleton.jsx";
 
